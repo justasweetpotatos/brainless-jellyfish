236ff829073eac53681d8553af9f3fc370e4a029
quanghuy <kehuydiet09094@gmail.com>
2024-09-29 22:04:11 +0700

    Recode ConnectionManager

--- src/manager/connectorManager.ts
+++ src/manager/connectorManager.ts
@@ -1,73 +1,46 @@
-import { Collection } from "discord.js";
-import { Connection } from "mysql";
-
+import { Connection, createConnection } from "mysql";
 import SuwaClient from "../bot";
-import Connector from "../database/connector";
-import Logger from "../utils/Logger";
 
-class ConnectionManager {
-  private connections: Collection<String, Connection>;
-  private adminConnection: Connection | null = null;
-  private connector: Connector;
-  private logger: Logger;
+interface ConnectionConfig {
+  host: string | "localhost";
+  username: string | "root";
+  password: string | "";
+  port: number | 3306;
+}
 
-  private host: string = "localhost";
-  private username: string = "root";
-  private password: string = "MySQLServer";
-  private dbPort: number = 3306;
-
-  private defaultConnectionConfig = {
-    host: this.host,
-    user: this.username,
-    password: this.password,
-    port: this.dbPort,
+class Connector {
+  private client: SuwaClient;
+  private readonly defaultConfig: ConnectionConfig = {
+    host: "localhost",
+    username: "root",
+    password: "root",
+    port: 3306,
   };
+  private config: ConnectionConfig;
+  private rootConnection: Connection;
 
-  public client: SuwaClient;
-
-  constructor(client: SuwaClient) {
+  constructor(client: SuwaClient, config: ConnectionConfig) {
     this.client = client;
-    this.connector = new Connector(client);
-    this.connections = new Collection();
-    this.logger = new Logger("COM-ConnectionManager", client.loggerFilePath);
+    this.config = config ?? this.defaultConfig;
+    this.rootConnection = this.createConnection(this.config);
   }
 
-  public getConnection(threadId: String) {
-    return this.connections.get(threadId);
+  public createConnection(config: ConnectionConfig) {
+    return createConnection(config);
   }
 
-  public forceCloseAllConnection() {
-    this.connections.forEach((conn) => conn.destroy());
-  }
-
-  async createMainConnection(): Promise<Connection> {
-    if (this.adminConnection) {
-      this.logger.log("Main connection already exists!");
-      return this.adminConnection;
-    }
-
-    try {
-      this.logger.log("Creating main connection...");
-
-      this.adminConnection = this.connector.createConnection({
-        host: this.host,
-        user: this.username,
-        password: this.password,
-        port: this.dbPort,
+  public async executeQuery(query: string, values: any[], connection?: Connection) {
+    return new Promise((resolve, reject) => {
+      connection = connection ?? this.rootConnection;
+      connection.query(query, values, (err, results) => {
+        if (err) {
+          this.logger.error(`Query execution failed: ${err}`);
+          return reject(err);
+        }
+        resolve(results);
       });
-
-      await this.connector.checkingConnection(this.adminConnection);
-
-      this.logger.success("Creat main connection Successfully !");
-
-      return this.adminConnection;
-    } catch (error) {
-      error instanceof Error
-        ? this.logger.error(`Create main connection failed: ${error.stack}`)
-        : this.logger.error("Undefined error !");
-      throw error;
-    }
+    });
   }
 }
 
-export default ConnectionManager;
+export default Connector;
